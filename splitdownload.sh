4027252783547283a6adee2fbd44ea5099ad48dd
Antony Ho <ntonyworkshop@gmail.com>
2013-12-21 00:32:34 +0800

    Add the shell script

---
+++ splitdownload.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#file name       :splitdownload.sh
+#description     :Accelerate file download by splitting download job using curl
+#author          :Antony Ho (http://antonyho.net/)
+#date            :20131221
+#usage           :./splitdownload.sh [FILE_URL]
+#===============================================================================
+
+
+# 100MB per trunk. Adjust it yourself
+TRUNKSIZE=$(echo "100 * 1024 * 1024" | bc)
+
+url=$1
+
+filesize=$(curl -sI $url | awk '/Content-Length/ { print $2 }' | tr -d $'\r')
+filename=$(curl -sI $url | awk -F= '/filename/ { print $2 }' | tr -d $'\r')
+
+echo "FILE NAME: $filename"
+echo "FILE SIZE: $filesize"
+
+numoftrunk=$(echo "scale = 0; $filesize / $TRUNKSIZE" | bc)
+
+echo "NUMBER OF THREADS: $numoftrunk"
+
+headbit=0
+tailbit=$(expr $TRUNKSIZE - 1)
+for i in `seq 1 $numoftrunk`
+do
+	curl -s --range $headbit-$tailbit -o $filename.part$i $url &
+	if [ "$tailbit" != "" ]; then
+		headbit=$(expr $tailbit + 1)
+	fi
+	if [ "$i" -eq $(expr $numoftrunk - 1) ]; then
+		tailbit=""
+	else
+		tailbit=$(expr $headbit + $TRUNKSIZE - 1)
+	fi
+done
