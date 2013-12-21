099a0a55938b320e7393e4ecb22dc8133ac7d45c
Rony <rony.yeung@gmail.com>
2013-12-22 00:20:19 +0800

    Update splitdownload.sh
    說真的，不少典型的 sh 壞習

--- splitdownload.sh
+++ splitdownload.sh
@@ -10,27 +10,30 @@
 # 100MB per trunk. Adjust it yourself
 TRUNKSIZE=$(echo 100*1024*1024 | bc)
 
-url=$1
+# or "$@"
+url="$1"
 
 filesize=$(curl -sI $url | awk '/Content-Length/ { print $2 }' | tr -d $'\r')
 filename=$(curl -sI $url | awk -F= '/filename/ { print $2 }' | tr -d $'\r')
 
-printf "FILE NAME: %s" $filename
-echo "FILE SIZE: $filesize"
+printf "FILE NAME: %s\n" $filename
+printf "FILE SIZE: %s\n" $filesize
 
-numoftrunk=$(echo "scale = 0; $filesize / $TRUNKSIZE" | bc)
+numoftrunk=$(echo ${filesize}/${TRUNKSIZE} | bc)
 
-printf "NUMBER OF THREADS: %s" $numoftrunk
+printf "NUMBER OF THREADS: %s\n" ${numoftrunk}
 
 headbit=0
-tailbit=$(expr $TRUNKSIZE - 1)
-for i in `seq 1 $numoftrunk`
+tailbit=$(expr ${TRUNKSIZE} - 1)
+for i in $(seq 1 ${numoftrunk})
 do
-	curl -s --range $headbit-$tailbit -o $filename.part$i $url &
-	if [ "$tailbit" != "" ]; then
+	curl -s --range ${headbit}-$tailbit -o ${filename}.part${i} $url &
+	if [ "$tailbit" != "" ]
+	then
 		headbit=$(expr $tailbit + 1)
 	fi
-	if [ "$i" -eq $(expr $numoftrunk - 1) ]; then
+	if [ "$i" -eq $(expr $numoftrunk - 1) ]
+	then
 		tailbit=""
 	else
 		tailbit=$(expr $headbit + $TRUNKSIZE - 1)
